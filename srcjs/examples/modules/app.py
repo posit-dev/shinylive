08c155054950eb173f8ce5e0157e59508f81ea5c
Winston Chang <winston@stdout.org>
2022-05-11 19:20:11 -0500

    Update module example app

--- srcjs/examples/modules/app.py
+++ srcjs/examples/modules/app.py
@@ -1,23 +1,22 @@
 from typing import Callable
-from htmltools import TagChildArg
 from shiny import *
-from shiny import modules
+
 
 # ============================================================
 # Counter module
 # ============================================================
-def counter_module_ui(
+def counter_ui(
     ns: Callable[[str], str], label: str = "Increment counter"
-) -> TagChildArg:
-    return ui.tags.div(
+) -> ui.TagChildArg:
+    return ui.div(
         {"style": "border: 1px solid #ccc; border-radius: 5px; margin: 5px 0;"},
-        ui.tags.h2("This is " + label),
+        ui.h2("This is " + label),
         ui.input_action_button(id=ns("button"), label=label),
         ui.output_text_verbatim(id=ns("out")),
     )
 
 
-def counter_module_server(input: Inputs, output: Outputs, session: Session):
+def counter_server(input: Inputs, output: Outputs, session: Session):
     count: reactive.Value[int] = reactive.Value(0)
 
     @reactive.Effect()
@@ -31,12 +30,12 @@ def counter_module_server(input: Inputs, output: Outputs, session: Session):
         return f"Click count is {count()}"
 
 
-counter_module = modules.Module(counter_module_ui, counter_module_server)
+counter_module = Module(counter_ui, counter_server)
 
 
-# ============================================================
-# App which uses module
-# ============================================================
+# =============================================================================
+# App that uses module
+# =============================================================================
 app_ui = ui.page_fluid(
     counter_module.ui("counter1", "Counter 1"),
     counter_module.ui("counter2", "Counter 2"),
@@ -48,4 +47,4 @@ def server(input: Inputs, output: Outputs, session: Session):
     counter_module.server("counter2")
 
 
-app = App(app_ui, server, debug=True)
+app = App(app_ui, server)
