f4dbe3744f2a0b4a5ca4a3f1ef076ed174790162
Winston Chang <winston@stdout.org>
2022-06-07 14:00:52 -0500

    Update reactive.Calc example

--- examples/reactive_calc/app.py
+++ examples/reactive_calc/app.py
@@ -1,18 +1,37 @@
-# A reactive Effect is run for its side effects, not for its return value. These
-# side effects can include printing messages to the console, writing files to
-# disk, or sending messages to a server.
+# A reactive Calc is used for its return value. It intelligently caches its value, and
+# only re-runs after it has been invalidated -- that is, when upstream reactive inputs
+# change.
 
 from shiny import *
 
 app_ui = ui.page_fluid(
-    ui.input_text("x", "Text input", placeholder="Enter text"),
+    ui.input_slider("x", "Choose a number", 1, 100, 50),
+    ui.output_text_verbatim("txt1"),
+    ui.output_text_verbatim("txt2"),
 )
 
 
 def server(input: Inputs, output: Outputs, session: Session):
-    @reactive.Effect()
-    def _():
-        print(f"x has changed to {input.x()}")
+    # Each time input.x() changes, it invalidates this reactive.Calc object. If someone
+    # then calls x_times_2(), it will execute the user function and return the value.
+    # The value is cached, so if another function calls x_times_2(), it will simply
+    # return the cached value, without re-running the function.  When input.x() changes
+    # again, it will invalidate this reactive.Calc, and the cache will be cleared.
+    @reactive.Calc()
+    def x_times_2():
+        val = input.x() * 2
+        print(f"Running x_times_2(). Result is {val}.")
+        return val
+
+    @output()
+    @render_text()
+    def txt1():
+        return f'x times 2 is: "{x_times_2()}"'
+
+    @output()
+    @render_text()
+    def txt2():
+        return f'x times 2 is: "{x_times_2()}"'
 
 
 app = App(app_ui, server)
