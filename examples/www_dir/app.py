6b8296e7305a5811f942d30b59ba796f93a230a7
Winston Chang <winston@stdout.org>
2022-06-07 23:09:34 -0500

    Add www_dir example

---
+++ examples/www_dir/app.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from shiny import ui, render, App, Inputs, Outputs, Session
+
+app_ui = ui.page_fluid(
+    ui.row(
+        ui.column(
+            6, ui.input_slider("n", "Make a Shiny square:", min=0, max=6, value=2)
+        ),
+        ui.column(
+            6,
+            ui.output_ui("images"),
+        ),
+    )
+)
+
+
+def square(x: ui.TagChildArg, n: int) -> ui.Tag:
+    row = ui.div([x] * n)
+    return ui.div([row] * n)
+
+
+def server(input: Inputs, output: Outputs, session: Session):
+    @output()
+    @render.ui()
+    def images() -> ui.Tag:
+        img = ui.img(src="logo.png", style="width: 40px;")
+        return square(img, input.n())
+
+
+www_dir = Path(__file__).parent / "www"
+app = App(app_ui, server, static_assets=www_dir)
