ae57a10bdacc711090909188754c65b8e8718e33
Winston Chang <winston@stdout.org>
2022-06-08 17:09:35 -0500

    Fix .svg import warnings

---
+++ src/types/custom.d.ts
@@ -0,0 +1,12 @@
+// This seems to be necessary so that the TS compiler doesn't complain about
+// importing svg files.
+declare module "*.svg" {
+  import * as React from "react";
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}
