c1cb2a0658044de026933447438b33de386fdddc
Winston Chang <winston@stdout.org>
2022-06-17 00:51:09 -0500

    Update file upload example to take binary files

--- examples/file_upload/app.py
+++ examples/file_upload/app.py
@@ -1,7 +1,9 @@
+from math import ceil
 from shiny import *
 
 app_ui = ui.page_fluid(
-    ui.input_file("file1", "Choose a text file to upload:", multiple=True),
+    ui.input_file("file1", "Choose a file to upload:", multiple=True),
+    ui.input_radio_buttons("type", "Type:", ["Text", "Binary"]),
     ui.output_text_verbatim("file_content"),
 )
 
@@ -25,11 +27,34 @@ def server(input: Inputs, output: Outputs, session: Session):
         # ]
         out_str = ""
         for file_info in file_infos:
-            out_str += "====== " + file_info["name"] + " ======\n"
-            with open(file_info["datapath"], "r") as f:
-                out_str += f.read()
+            out_str += "====== " + file_info["name"] + " (showing max 32kB) ======\n"
+            if input.type() == "Text":
+                with open(file_info["datapath"], "r") as f:
+                    out_str += f.read(32768)  # Read in at most 32kB
+            else:
+                with open(file_info["datapath"], "rb") as f:
+                    data = f.read(32768)
+                    out_str += format_hexdump(data)
 
         return out_str
 
 
-app = App(app_ui, server, debug=True)
+def format_hexdump(data: bytes) -> str:
+    hex_vals = ["{:02x}".format(b) for b in data]
+    hex_vals = group_into_blocks(hex_vals, 16)
+    hex_vals = [" ".join(row) for row in hex_vals]
+    hex_vals = "\n".join(hex_vals)
+    return hex_vals
+
+
+def group_into_blocks(x: list[str], blocksize: int):
+    """
+    Given a list, return a list of lists, where the inner lists each have `blocksize`
+    elements.
+    """
+    return [
+        x[i * blocksize : (i + 1) * blocksize] for i in range(ceil(len(x) / blocksize))
+    ]
+
+
+app = App(app_ui, server)
