bc0368ef159f8533b2e69d5bf78ac5db10383c6a
Winston Chang <winston@stdout.org>
2022-06-17 13:34:16 -0500

    Update extra packages app

--- examples/extra_packages/app.py
+++ examples/extra_packages/app.py
@@ -5,12 +5,26 @@ import attrs
 import tabulate
 
 app_ui = ui.page_fluid(
-    ui.div("See the requirements.txt file to see how to specify packages."),
+    ui.markdown(
+        """
+        This application doesn't actually do anything -- it simply demonstrates how to
+        import extra packages from PyPI, by using a `requirements.txt` file.
+
+        Packages listed in `requirements.txt` will be installed by micropip just before
+        the app is started. This means that each time someone visits the app, the
+        packages will be downloaded and installed into the browser's Pyodide
+        environment.
+
+        If you want test whether a package can be installed this way, either edit
+        `requirements.txt` and reload this app, or try running this in the terminal:
+
+        ```
+        import micropip
+        micropip.install("mypackage")
+        import mypackage
+        ```
+        """
+    ),
 )
 
-
-def server(input: Inputs, output: Outputs, session: Session):
-    pass
-
-
-app = App(app_ui, server, debug=True)
+app = App(app_ui, None)
