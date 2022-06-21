442485f0c2a881b18146f5d8faf9102dd25ca13b
Winston Chang <winston@stdout.org>
2022-06-21 16:03:12 -0500

    Update map example

--- examples/ipyleaflet/app.py
+++ examples/ipyleaflet/app.py
@@ -18,7 +18,7 @@ app_ui = ui.page_fluid(
 def server(input: Inputs, output: Outputs, session: Session):
 
     # Initialize and display when the session starts (1)
-    map = L.Map(center=(51.476852, -0.000500), zoom=12)
+    map = L.Map(center=(51.476852, -0.000500), zoom=12, scroll_wheel_zoom=True)
     register_widget("map", map)
 
     # When the slider changes, update the map's zoom attribute (2)
@@ -39,30 +39,11 @@ def server(input: Inputs, output: Outputs, session: Session):
         if len(center) == 0:
             return
 
-        lat = dms2str(dec2dms(center[0]))
+        lat = round(center[0], 5)
         lon = (center[1] + 180) % 360 - 180
-        lon = dms2str(dec2dms(lon))
+        lon = round(lon, 5)
 
         return ui.p(f"Latitude: {lat}", ui.br(), f"Longitude: {lon}")
-
-
-def dec2dms(dd: float):
-    negative = dd < 0
-    dd = abs(dd)
-    minutes, seconds = divmod(dd * 3600, 60)
-    degrees, minutes = divmod(minutes, 60)
-    if negative:
-        if degrees > 0:
-            degrees = -degrees
-        elif minutes > 0:
-            minutes = -minutes
-        else:
-            seconds = -seconds
-    return (degrees, minutes, seconds)
-
-
-def dms2str(dms: tuple[float, float, float], digits: int = 2):
-    return f"{round(dms[0])}\u00b0 {round(dms[1])}' {round(dms[2], digits)}''"
 
 
 app = App(app_ui, server)
