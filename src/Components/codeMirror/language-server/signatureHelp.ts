c7c2b60389f90b96614d3313b8d68d5fa657437a
Winston Chang <winston@stdout.org>
2022-07-03 00:23:00 -0500

    Restructure signatureHelp

--- src/Components/codeMirror/language-server/signatureHelp.ts
+++ src/Components/codeMirror/language-server/signatureHelp.ts
@@ -12,7 +12,7 @@ import {
   LanguageServerClient,
 } from "../../../language-server/client";
 import { LSPClient } from "../../../language-server/lsp-client";
-import { BaseLanguageServerView, clientFacet, uriFacet } from "./common";
+import { BaseLanguageServerView } from "./common";
 // import {
 //   DocSections,
 //   renderDocumentation,
@@ -20,7 +20,7 @@ import { BaseLanguageServerView, clientFacet, uriFacet } from "./common";
 // } from "./documentation";
 import { nameFromSignature, removeFullyQualifiedName } from "./names";
 import { offsetToPosition } from "./positions";
-import { StateEffect, StateField } from "@codemirror/state";
+import { Extension, StateEffect, StateField } from "@codemirror/state";
 import { showTooltip, Tooltip } from "@codemirror/view";
 import {
   Command,
@@ -121,31 +121,159 @@ const signatureHelpKeymap: readonly KeyBinding[] = [
   { key: "Escape", run: closeSignatureHelp },
 ];
 
+// =============================================================================
+// Signature help state field
+// =============================================================================
+const signatureHelpTooltipField = StateField.define<SignatureHelpState>({
+  create: () => ({
+    result: null,
+    tooltip: null,
+  }),
+  update(state, tr) {
+    // console.log("signatureHelp update", tr.effects);
+    for (const effect of tr.effects) {
+      if (effect.is(setSignatureHelpEffect)) {
+        console.log("signatureHelp ", effect);
+        return reduceSignatureHelpState(state, effect.value);
+      }
+    }
+    return state;
+  },
+  provide: (f) => showTooltip.from(f, (val) => val.tooltip),
+});
+
+const reduceSignatureHelpState = (
+  state: SignatureHelpState,
+  effect: SignatureChangeEffect
+): SignatureHelpState => {
+  console.log("reduceSignatureHelpState", effect);
+  if (state.tooltip && !effect.result) {
+    return {
+      result: null,
+      tooltip: null,
+    };
+  }
+  // It's a bit weird that we always update the position, but VS Code does this too.
+  // I think ideally we'd have a notion of "same function call". Does the
+  // node have a stable identity?
+  if (effect.result) {
+    const result = effect.result;
+    return {
+      result,
+      tooltip: {
+        pos: effect.pos,
+        above: true,
+        // This isn't great but the impact is really bad when it conflicts with autocomplete.
+        // strictSide: true,
+        create: () => {
+          const dom = document.createElement("div");
+          dom.className = "cm-signature-tooltip";
+          dom.appendChild(formatSignatureHelp(result));
+          return { dom };
+        },
+      },
+    };
+  }
+  return state;
+};
+
+const formatSignatureHelp = (help: SignatureHelp): Node => {
+  const { activeSignature: activeSignatureIndex, signatures } = help;
+  // We intentionally do something minimal here to minimise distraction.
+  const activeSignature =
+    activeSignatureIndex === null
+      ? signatures[0]
+      : signatures[activeSignatureIndex!];
+  const {
+    label,
+    parameters,
+    documentation: signatureDoc,
+    activeParameter: activeParameterIndex,
+  } = activeSignature;
+  const activeParameter =
+    activeParameterIndex !== undefined && parameters
+      ? parameters[activeParameterIndex]
+      : undefined;
+  const activeParameterLabel = activeParameter?.label;
+  const activeParameterDoc = activeParameter?.documentation;
+  if (typeof activeParameterLabel === "string") {
+    throw new Error("Not supported");
+  }
+  let from = label.length;
+  let to = label.length;
+  if (Array.isArray(activeParameterLabel)) {
+    [from, to] = activeParameterLabel;
+  }
+  return formatHighlightedParameter(
+    label,
+    from,
+    to,
+    signatureDoc,
+    activeParameterDoc
+  );
+};
+
+const formatHighlightedParameter = (
+  label: string,
+  from: number,
+  to: number,
+  signatureDoc: string | MarkupContent | undefined,
+  activeParameterDoc: string | MarkupContent | undefined
+): Node => {
+  let before = label.substring(0, from);
+  const id = nameFromSignature(before);
+  const parameter = label.substring(from, to);
+  const after = label.substring(to);
+
+  // Do this after using the indexes, not to the original label.
+  before = removeFullyQualifiedName(before);
+
+  const parent = document.createElement("div");
+  parent.className = "docs-spacing";
+  const signature = parent.appendChild(document.createElement("code"));
+  signature.className = "cm-signature-signature";
+  signature.appendChild(document.createTextNode(before));
+  const span = signature.appendChild(document.createElement("span"));
+  span.className = "cm-signature-activeParameter";
+  span.appendChild(document.createTextNode(parameter));
+  signature.appendChild(document.createTextNode(after));
+  parent.appendChild(document.createElement("hr"));
+
+  // if (activeParameterDoc) {
+  //   parent.appendChild(renderDocumentation(
+  //     activeParameterDoc,
+  //     DocSections.All
+  //   ));
+  //   parent.appendChild(renderDocumentation(
+  //     signatureDoc,
+  //     DocSections.Example
+  //   ));
+  // } else {
+  //   // No params so show summary and example from the signature docstring.
+  //   parent.appendChild(renderDocumentation(
+  //     signatureDoc,
+  //     DocSections.Summary | DocSections.Example
+  //   ));
+  // }
+
+  // return wrapWithDocumentationButton( parent, id);
+  return parent;
+};
+
+// =============================================================================
+// Extension
+// =============================================================================
+
+/**
+ * Create a signatureHelp Extension.
+ */
 export const signatureHelp = (
   lspClient: LSPClient,
   filename: string,
   automatic: boolean
-) => {
+): Extension => {
   const client = lspClient.client;
   const uri = createUri(filename);
-
-  const signatureHelpTooltipField = StateField.define<SignatureHelpState>({
-    create: () => ({
-      result: null,
-      tooltip: null,
-    }),
-    update(state, tr) {
-      // console.log("signatureHelp update", tr.effects);
-      for (const effect of tr.effects) {
-        if (effect.is(setSignatureHelpEffect)) {
-          console.log("signatureHelp ", effect);
-          return reduceSignatureHelpState(state, effect.value);
-        }
-      }
-      return state;
-    },
-    provide: (f) => showTooltip.from(f, (val) => val.tooltip),
-  });
 
   class SignatureHelpView
     extends BaseLanguageServerView
@@ -176,124 +304,6 @@ export const signatureHelp = (
       }
     }
   }
-
-  const reduceSignatureHelpState = (
-    state: SignatureHelpState,
-    effect: SignatureChangeEffect
-  ): SignatureHelpState => {
-    console.log("reduceSignatureHelpState", effect);
-    if (state.tooltip && !effect.result) {
-      return {
-        result: null,
-        tooltip: null,
-      };
-    }
-    // It's a bit weird that we always update the position, but VS Code does this too.
-    // I think ideally we'd have a notion of "same function call". Does the
-    // node have a stable identity?
-    if (effect.result) {
-      const result = effect.result;
-      return {
-        result,
-        tooltip: {
-          pos: effect.pos,
-          above: true,
-          // This isn't great but the impact is really bad when it conflicts with autocomplete.
-          // strictSide: true,
-          create: () => {
-            const dom = document.createElement("div");
-            dom.className = "cm-signature-tooltip";
-            dom.appendChild(formatSignatureHelp(result));
-            return { dom };
-          },
-        },
-      };
-    }
-    return state;
-  };
-
-  const formatSignatureHelp = (help: SignatureHelp): Node => {
-    const { activeSignature: activeSignatureIndex, signatures } = help;
-    // We intentionally do something minimal here to minimise distraction.
-    const activeSignature =
-      activeSignatureIndex === null
-        ? signatures[0]
-        : signatures[activeSignatureIndex!];
-    const {
-      label,
-      parameters,
-      documentation: signatureDoc,
-      activeParameter: activeParameterIndex,
-    } = activeSignature;
-    const activeParameter =
-      activeParameterIndex !== undefined && parameters
-        ? parameters[activeParameterIndex]
-        : undefined;
-    const activeParameterLabel = activeParameter?.label;
-    const activeParameterDoc = activeParameter?.documentation;
-    if (typeof activeParameterLabel === "string") {
-      throw new Error("Not supported");
-    }
-    let from = label.length;
-    let to = label.length;
-    if (Array.isArray(activeParameterLabel)) {
-      [from, to] = activeParameterLabel;
-    }
-    return formatHighlightedParameter(
-      label,
-      from,
-      to,
-      signatureDoc,
-      activeParameterDoc
-    );
-  };
-
-  const formatHighlightedParameter = (
-    label: string,
-    from: number,
-    to: number,
-    signatureDoc: string | MarkupContent | undefined,
-    activeParameterDoc: string | MarkupContent | undefined
-  ): Node => {
-    let before = label.substring(0, from);
-    const id = nameFromSignature(before);
-    const parameter = label.substring(from, to);
-    const after = label.substring(to);
-
-    // Do this after using the indexes, not to the original label.
-    before = removeFullyQualifiedName(before);
-
-    const parent = document.createElement("div");
-    parent.className = "docs-spacing";
-    const signature = parent.appendChild(document.createElement("code"));
-    signature.className = "cm-signature-signature";
-    signature.appendChild(document.createTextNode(before));
-    const span = signature.appendChild(document.createElement("span"));
-    span.className = "cm-signature-activeParameter";
-    span.appendChild(document.createTextNode(parameter));
-    signature.appendChild(document.createTextNode(after));
-    parent.appendChild(document.createElement("hr"));
-
-    // if (activeParameterDoc) {
-    //   parent.appendChild(renderDocumentation(
-    //     activeParameterDoc,
-    //     DocSections.All
-    //   ));
-    //   parent.appendChild(renderDocumentation(
-    //     signatureDoc,
-    //     DocSections.Example
-    //   ));
-    // } else {
-    //   // No params so show summary and example from the signature docstring.
-    //   parent.appendChild(renderDocumentation(
-    //     signatureDoc,
-    //     DocSections.Summary | DocSections.Example
-    //   ));
-    // }
-
-    // return wrapWithDocumentationButton( parent, id);
-    return parent;
-  };
 
   return [
     // View only handles automatic triggering.
