f08bb48c6f265b7e31279dd66fc07a60453aa581
Winston Chang <winston@stdout.org>
2022-07-18 09:30:57 -0500

    Use shinylive.io for sharing

--- src/Components/share.ts
+++ src/Components/share.ts
@@ -1,8 +1,8 @@
 import { FCtoFCJSON, FileContent } from "./filecontent";
 import LZString from "lz-string";
 
-export const editorUrlPrefix = "https://pyshiny.netlify.app/editor/#code=";
-export const appUrlPrefix = "https://pyshiny.netlify.app/app/#code=";
+export const editorUrlPrefix = "https://shinylive.io/editor/#code=";
+export const appUrlPrefix = "https://shinylive.io/app/#code=";
 
 /**
  * Given a FileContent[] object, return a string that is a LZ-compressed JSON
