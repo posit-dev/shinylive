d22ce513dafe8f9d2b8b37a06e2ebf556ae2daa2
Nick Strayer <nick.strayer@gmail.com>
2022-08-15 16:52:21 -0700

    Add basic test for if editor files can be loaded from compressed url string

---
+++ playwright/load-from-url.spec.ts
@@ -0,0 +1,21 @@
+import { click_run_app_button, wait_until_initialized } from "./helpers";
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto(
+    "http://localhost:3000/editor/#code=NobwRAdghgtgpmAXGALnAzigdABwJ5gA0YAxgPYRqVJg4BOAlpQBQA6YAKgBYPoAEvPuQAmcPgDM6ZGHxRcxAVzoAbdgEowAXwC6QA"
+  );
+
+  // Wait for initialization to complete
+  await wait_until_initialized(page);
+  // Make sure the correct text is in the editor
+  await page.locator(`.Editor text=print("This is code from the url")`);
+
+  // This is a bit of an intense selector for the run button but right now I
+  // cant figure out a better way
+  await click_run_app_button(page);
+
+  await expect(
+    page.locator(`text=This is code from the url >> nth=2`)
+  ).toBeVisible();
+});
