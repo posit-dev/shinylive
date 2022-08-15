01ac71c1da374b2b55d24291af0190e959ae8a20
Nick Strayer <nick.strayer@gmail.com>
2022-08-15 15:40:28 -0700

    Make playwright in charge of running dev-server

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,10 +70,10 @@ const config: PlaywrightTestConfig = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: "make serve",
+    port: 3000,
+  },
 };
 
 export default config;
