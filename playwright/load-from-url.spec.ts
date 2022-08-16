6650484d13dbf7eb076091911dce56a93b2cbfe6
Nick Strayer <nick.strayer@gmail.com>
2022-08-16 12:04:08 -0700

    Use baseurl playwright config to clean up url construction

--- playwright/load-from-url.spec.ts
+++ playwright/load-from-url.spec.ts
@@ -1,8 +1,4 @@
-import {
-  expect_app_has_text,
-  expect_editor_has_text,
-  makeUrl,
-} from "./helpers";
+import { expect_app_has_text, expect_editor_has_text } from "./helpers";
 import { expect, test } from "@playwright/test";
 
 const app_url_encoding =
@@ -10,7 +6,7 @@ const app_url_encoding =
 
 test.describe("The URL can be used to load data", async () => {
   test("Editor view", async ({ page }) => {
-    await page.goto(makeUrl({ view: "editor", data_param: app_url_encoding }));
+    await page.goto(`/editor/#${app_url_encoding}`);
 
     // Make sure the correct text is in the editor
     await expect_editor_has_text(page, 'ui.h1("Code from a url")');
@@ -19,9 +15,7 @@ test.describe("The URL can be used to load data", async () => {
   });
 
   test("Examples view", async ({ page }) => {
-    await page.goto(
-      makeUrl({ view: "examples", data_param: app_url_encoding })
-    );
+    await page.goto(`/examples/#${app_url_encoding}`);
 
     // Sanity check we're in the examples view.
     await expect(page.locator("text=/^examples$/i")).toBeVisible();
@@ -32,41 +26,36 @@ test.describe("The URL can be used to load data", async () => {
     await expect_app_has_text(page, "Code from a url", "h1");
   });
 
-  test("App view with header bar", async ({ page }) => {
-    await page.goto(makeUrl({ view: "app", data_param: app_url_encoding }));
+  test("App view", async ({ page }) => {
+    await page.goto(`/app/#${app_url_encoding}`);
 
     await expect_app_has_text(page, "Code from a url", "h1");
   });
 });
 
+// Looks for the shiny logo
+const header_bar_selector = '.HeaderBar img[alt="Shiny"]';
+
 test.describe("The header bar parameter can turn off the header in app view but not the other views", async () => {
   test("Editor view can't turn off header bar", async ({ page }) => {
-    await page.goto(
-      makeUrl({ view: "editor", header: false, data_param: app_url_encoding })
-    );
+    await page.goto(`/editor/#h=0&${app_url_encoding}`);
 
-    await expect(page.locator('.HeaderBar img[alt="Shiny"]')).toBeVisible();
+    await expect(page.locator(header_bar_selector)).toBeVisible();
   });
   test("Examples view can't turn off header bar", async ({ page }) => {
-    await page.goto(
-      makeUrl({ view: "examples", header: false, data_param: app_url_encoding })
-    );
+    await page.goto(`/examples/#h=0&${app_url_encoding}`);
 
-    await expect(page.locator('.HeaderBar img[alt="Shiny"]')).toBeVisible();
+    await expect(page.locator(header_bar_selector)).toBeVisible();
   });
 
   test("App view can turn off header bar", async ({ page }) => {
-    await page.goto(
-      makeUrl({ view: "app", header: false, data_param: app_url_encoding })
-    );
+    await page.goto(`/app/#h=0&${app_url_encoding}`);
 
-    await expect(page.locator('.HeaderBar img[alt="Shiny"]')).not.toBeVisible();
+    await expect(page.locator(header_bar_selector)).not.toBeVisible();
   });
-  test("App view can showheader bar", async ({ page }) => {
-    await page.goto(
-      makeUrl({ view: "app", header: true, data_param: app_url_encoding })
-    );
+  test("App view can show header bar", async ({ page }) => {
+    await page.goto(`/editor/#${app_url_encoding}`);
 
-    await expect(page.locator('.HeaderBar img[alt="Shiny"]')).toBeVisible();
+    await expect(page.locator(header_bar_selector)).toBeVisible();
   });
 });
