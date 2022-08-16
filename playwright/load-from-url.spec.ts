3a9d364b4e4b3813c54298b4c810e58927a35dfc
Nick Strayer <nick.strayer@gmail.com>
2022-08-16 10:47:51 -0700

    Split tests for header bar visibility into separate describe section

--- playwright/load-from-url.spec.ts
+++ playwright/load-from-url.spec.ts
@@ -18,25 +18,55 @@ test.describe("The URL can be used to load data", async () => {
     await expect_app_has_text(page, "Code from a url", "h1");
   });
 
-  test("Default app view with header bar", async ({ page }) => {
+  test("Examples view", async ({ page }) => {
+    await page.goto(
+      makeUrl({ view: "examples", data_param: app_url_encoding })
+    );
+
+    // Sanity check we're in the examples view.
+    await expect(page.locator("text=/^examples$/i")).toBeVisible();
+
+    // Make sure the correct text is in the editor
+    await expect_editor_has_text(page, 'ui.h1("Code from a url")');
+
+    await expect_app_has_text(page, "Code from a url", "h1");
+  });
+
+  test("App view with header bar", async ({ page }) => {
     await page.goto(makeUrl({ view: "app", data_param: app_url_encoding }));
 
     await expect_app_has_text(page, "Code from a url", "h1");
+  });
+});
 
-    await expect(
-      page.locator('[aria-label="Open a copy in editor view"]')
-    ).toBeVisible();
+test.describe("The header bar parameter can turn off the header in app view but not the other views", async () => {
+  test("Editor view can't turn off header bar", async ({ page }) => {
+    await page.goto(
+      makeUrl({ view: "editor", header: false, data_param: app_url_encoding })
+    );
+
+    await expect(page.locator('.HeaderBar img[alt="Shiny"]')).toBeVisible();
+  });
+  test("Examples view can't turn off header bar", async ({ page }) => {
+    await page.goto(
+      makeUrl({ view: "examples", header: false, data_param: app_url_encoding })
+    );
+
+    await expect(page.locator('.HeaderBar img[alt="Shiny"]')).toBeVisible();
   });
 
-  test("App view without header bar", async ({ page }) => {
+  test("App view can turn off header bar", async ({ page }) => {
     await page.goto(
       makeUrl({ view: "app", header: false, data_param: app_url_encoding })
     );
 
-    await expect_app_has_text(page, "Code from a url", "h1");
+    await expect(page.locator('.HeaderBar img[alt="Shiny"]')).not.toBeVisible();
+  });
+  test("App view can showheader bar", async ({ page }) => {
+    await page.goto(
+      makeUrl({ view: "app", header: true, data_param: app_url_encoding })
+    );
 
-    await expect(
-      page.locator('[aria-label="Open a copy in editor view"]')
-    ).not.toBeVisible();
+    await expect(page.locator('.HeaderBar img[alt="Shiny"]')).toBeVisible();
   });
 });
