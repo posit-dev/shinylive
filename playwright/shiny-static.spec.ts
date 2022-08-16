87def82199683138e7fe8470b26a10078ee2c84a
Nick Strayer <nick.strayer@gmail.com>
2022-08-16 12:36:59 -0700

    Add test for the edit view as well

--- playwright/shiny-static.spec.ts
+++ playwright/shiny-static.spec.ts
@@ -1,8 +1,22 @@
+import { expect_editor_has_text } from "./helpers";
 import { expect, test } from "@playwright/test";
 
-test.describe("The URL can be used to load data", async () => {
-  test("Editor view", async ({ page }) => {
-    await page.goto(`http://localhost:8008/`);
+test.use({ baseURL: "http://localhost:8008" });
+
+test.describe("Shiny-Static deploys", async () => {
+  test("Main app view is base of URL", async ({ page }) => {
+    await page.goto(`/`);
+
+    await expect(
+      page.frameLocator(".app-frame").locator("text=Hello Shiny-Static!")
+    ).toBeVisible();
+  });
+
+  test("Edit view is accessable via the edit path", async ({ page }) => {
+    await page.goto(`/edit`);
+
+    // Make sure editor is there and has the identifying header
+    await expect_editor_has_text(page, `ui.h2("Hello Shiny-Static!")`);
 
     await expect(
       page.frameLocator(".app-frame").locator("text=Hello Shiny-Static!")
