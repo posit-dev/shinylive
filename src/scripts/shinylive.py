ce0753beb1434f47a6d4649518c45fc4b98e4967
Winston Chang <winston@stdout.org>
2022-08-19 16:22:02 -0500

    Add _shinylive_base_deps function

--- src/scripts/shinylive.py
+++ src/scripts/shinylive.py
@@ -7,7 +7,7 @@ import shutil
 import sys
 from pathlib import Path
 from textwrap import dedent
-from typing import Callable, Dict, List, Literal, Set, Tuple, TypedDict, Union
+from typing import Callable, Dict, List, Literal, Set, Tuple, TypedDict, Union, cast
 
 from typing_extensions import NotRequired
 
@@ -182,6 +182,97 @@ To upgrade shinylive, run these commands:
 # =============================================================================
 # Utility functions
 # =============================================================================
+def _shinylive_base_deps(path_prefix: str = "shinylive-dist/") -> None:
+    """
+    Prints out the base dependencies that are always included in a deployment.
+    """
+
+    all_files: List[str] = []
+    for root, dirs, files in os.walk(shinylive_dir):
+        root = Path(root)
+        rel_root = root.relative_to(shinylive_dir)
+        if rel_root == Path("."):
+            dirs.remove("scripts")
+        elif rel_root == Path("shinylive"):
+            dirs.remove("shiny_static")
+            files.remove("examples.json")
+        elif rel_root == Path("shinylive/pyodide"):
+            dirs.remove("fonts")
+            files[:] = BASE_PYODIDE_FILES
+
+        for file in files:
+            if file.startswith("."):
+                continue
+            all_files.append(str(rel_root / file))
+
+    scripts: List[Dict[str, Union[str, Dict[str, str]]]] = []
+    stylesheets: List[Dict[str, Union[str, Dict[str, str]]]] = []
+    resources: List[Dict[str, str]] = []
+
+    for file in all_files:
+        if os.path.basename(file) in [
+            "load-serviceworker.js",
+            "jquery.min.js",
+            "jquery.terminal.min.js",
+            "run-python-blocks.js",
+        ]:
+            script_item: Dict[str, Union[str, Dict[str, str]]] = {
+                "name": file,
+                "path": os.path.join(path_prefix, file),
+            }
+
+            if os.path.basename(file) in [
+                "load-serviceworker.js",
+                "run-python-blocks.js",
+            ]:
+                script_item["attribs"] = {"type": "module"}
+
+            scripts.append(script_item)
+
+        if os.path.basename(file) in [
+            "jquery.terminal.min.css",
+            "shinylive.css",
+        ]:
+            stylesheets.append(
+                {
+                    "name": file,
+                    "path": os.path.join(path_prefix, file),
+                }
+            )
+        else:
+            resources.append(
+                {
+                    "name": file,
+                    "path": os.path.join(path_prefix, file),
+                }
+            )
+
+    # Sort scripts so that load-serviceworker.js is first, and run-python-blocks.js is
+    # last.
+    def scripts_sort_fun(x: Dict[str, Union[str, Dict[str, str]]]) -> int:
+        filename = os.path.basename(cast(str, x["name"]))
+        if filename == "load-serviceworker.js":
+            return 0
+        elif filename == "run-python-blocks.js":
+            return 2
+        else:
+            return 1
+
+    scripts.sort(key=scripts_sort_fun)
+
+    print(
+        json.dumps(
+            {
+                "scripts": scripts,
+                "stylesheets": stylesheets,
+                "resources": resources,
+            },
+            indent=2,
+        )
+    )
+    return
+
+
 def _copy_pyodide_deps(
     json_file: Union[str, Path],
     destdir: Union[str, Path],
