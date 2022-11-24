9e14fbd2796b41ccfcc723c4c71937a21794d6a5
Liam Osler <osler.liam@gmail.com>
2022-11-24 18:25:28 -0400

    change searchObject variable name

--- examples/location_search/app.py
+++ examples/location_search/app.py
@@ -3,9 +3,9 @@ from shiny import App, render, ui
 app_ui = ui.page_fluid(
     ui.tags.script(
        """
-        let searchObject = {};
         let search =  window.parent.location.search.substring(1);
         let searchArray = search.split('&');
+        let searchObject = {};
         for(item of searchArray){
             let array = item.split("=");
             let key = array[0];
@@ -14,11 +14,11 @@ app_ui = ui.page_fluid(
         }
         
         $(document).on('shiny:connected', function(event) {
-            Shiny.setInputValue("searchString", searchObject);
+            Shiny.setInputValue("searchObject", searchObject);
         }); 
        """
     ),
-    ui.h2("Location Search"),
+    ui.h2("Window Location Search String"),
     ui.output_text_verbatim("search"),
 )
 
@@ -26,6 +26,6 @@ def server(input, output, session):
     @output
     @render.text
     def search():
-        return f"Location search is : {input.searchString()}"
+        return f"Location search is : {input.searchObject()}"
 
 app = App(app_ui, server)
