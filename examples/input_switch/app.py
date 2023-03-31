b476470a23334c7ec1c48b03c9354bcf1e20c27c
Winston Chang <winston@stdout.org>
2023-03-31 17:46:00 -0500

    Add input_switch example

---
+++ examples/input_switch/app.py
@@ -0,0 +1,16 @@
+from shiny import App, render, ui
+
+app_ui = ui.page_fluid(
+    ui.input_switch("x", "Switch input"),
+    ui.output_text_verbatim("txt"),
+)
+
+
+def server(input, output, session):
+    @output
+    @render.text
+    def txt():
+        return f"x: {input.x()}"
+
+
+app = App(app_ui, server, debug=True)
