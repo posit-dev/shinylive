5e2563477972bbf17c5bbf6026e4904231e7cecc
Winston Chang <winston@stdout.org>
2023-04-18 22:04:46 -0500

    Move non-app to bottom

--- examples/hello_world/hello.py
+++ examples/hello_world/hello.py
@@ -1,4 +1,5 @@
-# Python code that is not part of a Shiny app can also be used for examples.
+# Python code that is not part of a Shiny app can also be used for examples
+# in Shinylive.
 # You can run this line-by-line by pressing cmd- or ctrl-Enter, or you can
 # select a block of text and run it with cmd-/ctrl-Enter.
 def hello():
