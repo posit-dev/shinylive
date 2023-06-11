7277dd13f3a0adae0a1a22be72f78b117fadea30
Winston Chang <winston@stdout.org>
2023-06-10 22:17:38 -0500

    Workflow updates

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,10 @@
-import packageJson from "../package.json";
-import buildExamples from "./build_examples_json";
 import { spawn } from "child_process";
 import esbuild from "esbuild";
 import * as fs from "fs";
 import http from "http";
 import process from "process";
+import packageJson from "../package.json";
+import buildExamples from "./build_examples_json";
 
 const EXAMPLES_SOURCE_DIR = "./examples";
 const BUILD_DIR = "./build";
@@ -51,150 +51,145 @@ const rebuildLoggerPlugin = {
     build.onStart(() => {
       console.log(`[${new Date().toISOString()}] Rebuilding JS files...`);
     });
-  }
-}
+  },
+};
 
 const buildmap = {
-  'app':
-    esbuild.context({
-      bundle: true,
-      entryPoints: {
-        shinylive: "src/Components/App.tsx",
-        Editor: "src/Components/Editor.tsx",
-      },
-      outdir: `${SITE_DIR}/shinylive/`,
-      // All of these are dynamic imports in pyodide.mjs (which we copied to
-      // src/pyodide/pyodide.js). It will never actually do these imports, so
-      // we'll mark them as external so esbuild doesn't try to bundle them.
-      external: [
-        "node-fetch",
-        "path",
-        "fs",
-        "vm",
-        "crypto",
-        "child_process",
-        "url",
-        "ws",
-      ],
-      format: "esm",
-      target: "es2020",
-      splitting: true,
-      // It would be more organized to put the chunks in "chunks/[name]", but this
-      // causes problems when ../pyodide/pyodide.js loads "pyodide_py.tar" -- it
-      // looks in /shinylive/chunks/pyodide/pyodide_py.tar, which doesn't exist.
-      // This probably has something to do with how pyodide.js specifies the path.
-      chunkNames: "[name]-[hash]",
-      minify: minify,
-      banner: banner,
-      metafile: metafile,
-      define: {
-        "process.env.NODE_ENV": reactProductionMode
-          ? '"production"'
-          : '"development"',
-      },
-      loader: {
-        ".svg": "dataurl",
-      },
-      plugins: [
-        rebuildLoggerPlugin,
-        {
-          // This removes previously-built chunk-[hash].js files so that they
-          // don't clutter up the build directory.
-          name: "chunk-cleaner",
-          setup(build) {
-            build.onStart(async () => {
-              fs.readdirSync(`${BUILD_DIR}/shinylive/`)
-                .filter((file) => file.startsWith("chunk-"))
-                .forEach((file) => {
-                  fs.unlinkSync(`${BUILD_DIR}/shinylive/${file}`);
-                });
-            });
-          },
+  app: esbuild.context({
+    bundle: true,
+    entryPoints: {
+      shinylive: "src/Components/App.tsx",
+      Editor: "src/Components/Editor.tsx",
+    },
+    outdir: `${SITE_DIR}/shinylive/`,
+    // All of these are dynamic imports in pyodide.mjs (which we copied to
+    // src/pyodide/pyodide.js). It will never actually do these imports, so
+    // we'll mark them as external so esbuild doesn't try to bundle them.
+    external: [
+      "node-fetch",
+      "path",
+      "fs",
+      "vm",
+      "crypto",
+      "child_process",
+      "url",
+      "ws",
+    ],
+    format: "esm",
+    target: "es2020",
+    splitting: true,
+    // It would be more organized to put the chunks in "chunks/[name]", but this
+    // causes problems when ../pyodide/pyodide.js loads "pyodide_py.tar" -- it
+    // looks in /shinylive/chunks/pyodide/pyodide_py.tar, which doesn't exist.
+    // This probably has something to do with how pyodide.js specifies the path.
+    chunkNames: "[name]-[hash]",
+    minify: minify,
+    banner: banner,
+    metafile: metafile,
+    define: {
+      "process.env.NODE_ENV": reactProductionMode
+        ? '"production"'
+        : '"development"',
+    },
+    loader: {
+      ".svg": "dataurl",
+    },
+    plugins: [
+      rebuildLoggerPlugin,
+      {
+        // This removes previously-built chunk-[hash].js files so that they
+        // don't clutter up the build directory.
+        name: "chunk-cleaner",
+        setup(build) {
+          build.onStart(async () => {
+            fs.readdirSync(`${BUILD_DIR}/shinylive/`)
+              .filter((file) => file.startsWith("chunk-"))
+              .forEach((file) => {
+                fs.unlinkSync(`${BUILD_DIR}/shinylive/${file}`);
+              });
+          });
         },
-        {
-          name: "example-builder",
-          setup(build) {
-            build.onStart(() => {
-              // On every rebuild make sure the examples are up to date.
-              // One issue is this won't force esbuild to watch for the changes
-              // of the example files themselves so live-reloading won't work
-              buildExamples(EXAMPLES_SOURCE_DIR, BUILD_DIR);
-            });
-          },
+      },
+      {
+        name: "example-builder",
+        setup(build) {
+          build.onStart(() => {
+            // On every rebuild make sure the examples are up to date.
+            // One issue is this won't force esbuild to watch for the changes
+            // of the example files themselves so live-reloading won't work
+            buildExamples(EXAMPLES_SOURCE_DIR, BUILD_DIR);
+          });
         },
-      ],
-    }),
-  'worker':
-    esbuild.context({
-      bundle: true,
-      entryPoints: [
-        "src/pyodide-worker.ts",
-        "src/load-shinylive-sw.ts",
-        "src/run-python-blocks.ts",
-      ],
-      outdir: `${BUILD_DIR}/shinylive`,
-      // See note in esbuild.build() call above about why these are external.
-      external: [
-        "node-fetch",
-        "path",
-        "fs",
-        "vm",
-        "crypto",
-        "child_process",
-        "url",
-        // shinylive.js is used in run-python-blocks.ts, but we don't want to
-        // bundle it.
-        "./shinylive.js",
-      ],
-      format: "esm",
-      target: "es2020",
-      minify: minify,
-      banner: banner,
-      plugins: [ rebuildLoggerPlugin ],
-    }),
-  'codeblock-to-json':
-    esbuild.context({
-      bundle: true,
-      entryPoints: ["src/scripts/codeblock-to-json.ts"],
-      outdir: `${BUILD_DIR}/scripts`,
-      format: "esm",
-      target: "es2022",
-      minify: minify,
-      banner: banner,
-      plugins: [ rebuildLoggerPlugin ],
-    }),
+      },
+    ],
+  }),
+  worker: esbuild.context({
+    bundle: true,
+    entryPoints: [
+      "src/pyodide-worker.ts",
+      "src/load-shinylive-sw.ts",
+      "src/run-python-blocks.ts",
+    ],
+    outdir: `${BUILD_DIR}/shinylive`,
+    // See note in esbuild.build() call above about why these are external.
+    external: [
+      "node-fetch",
+      "path",
+      "fs",
+      "vm",
+      "crypto",
+      "child_process",
+      "url",
+      // shinylive.js is used in run-python-blocks.ts, but we don't want to
+      // bundle it.
+      "./shinylive.js",
+    ],
+    format: "esm",
+    target: "es2020",
+    minify: minify,
+    banner: banner,
+    plugins: [rebuildLoggerPlugin],
+  }),
+  "codeblock-to-json": esbuild.context({
+    bundle: true,
+    entryPoints: ["src/scripts/codeblock-to-json.ts"],
+    outdir: `${BUILD_DIR}/scripts`,
+    format: "esm",
+    target: "es2022",
+    minify: minify,
+    banner: banner,
+    plugins: [rebuildLoggerPlugin],
+  }),
   // Compile src/shinylive-inject-socket.ts to
   // src/assets/shinylive-inject-socket.txt. That file is in turn ingested into
   // shinylive-sw.js.
-  'shinylive-inject-socket':
-    esbuild.context({
-      bundle: true,
-      entryPoints: ["src/shinylive-inject-socket.ts"],
-      outfile: "src/assets/shinylive-inject-socket.txt",
-      format: "esm",
-      target: "es2020",
-      // Don't minify, because the space savings are minimal, and the it will lead
-      // to spurious diffs when building for dev vs. prod.
-      minify: false,
-      plugins: [ rebuildLoggerPlugin ],
-    }),
-  'shinylive-sw':
-    esbuild.context({
-      bundle: true,
-      entryPoints: ["src/shinylive-sw.ts"],
-      outdir: `${BUILD_DIR}`,
-      format: "esm",
-      target: "es2020",
-      minify: minify,
-      banner: banner,
-      plugins: [ rebuildLoggerPlugin ],
-    })
+  "shinylive-inject-socket": esbuild.context({
+    bundle: true,
+    entryPoints: ["src/shinylive-inject-socket.ts"],
+    outfile: "src/assets/shinylive-inject-socket.txt",
+    format: "esm",
+    target: "es2020",
+    // Don't minify, because the space savings are minimal, and the it will lead
+    // to spurious diffs when building for dev vs. prod.
+    minify: false,
+    plugins: [rebuildLoggerPlugin],
+  }),
+  "shinylive-sw": esbuild.context({
+    bundle: true,
+    entryPoints: ["src/shinylive-sw.ts"],
+    outdir: `${BUILD_DIR}`,
+    format: "esm",
+    target: "es2020",
+    minify: minify,
+    banner: banner,
+    plugins: [rebuildLoggerPlugin],
+  }),
 };
 
 Object.values(buildmap).forEach((build) =>
   build
     .then((context) => {
-      context.rebuild()
+      context.rebuild();
       return context;
     })
     .then((context) => {
@@ -204,16 +199,16 @@ Object.values(buildmap).forEach((build) =>
 );
 
 if (metafile) {
-  buildmap['app']
+  buildmap["app"]
     .then((context) => context.rebuild())
     .then((result) => {
       fs.writeFileSync("esbuild-meta.json", JSON.stringify(result.metafile));
     })
-    .catch(() => process.exit(1))
+    .catch(() => process.exit(1));
 }
 
 if (serve) {
-  buildmap['app']
+  buildmap["app"]
     .then((context) => {
       context.serve({ servedir: SITE_DIR, port: 3001 }).then(() => {
         http
@@ -243,9 +238,11 @@ if (serve) {
                     res.writeHead(proxyRes.statusCode!, newHeaders);
                     res.write(jsReloadCode);
                   } else {
-                    const client = res.writeHead(proxyRes.statusCode!, proxyRes.headers);
-                    if (req.url === "/esbuild")
-                      clients.push(client);
+                    const client = res.writeHead(
+                      proxyRes.statusCode!,
+                      proxyRes.headers
+                    );
+                    if (req.url === "/esbuild") clients.push(client);
                   }
                   proxyRes.pipe(res, { end: true });
                 }
@@ -263,9 +260,11 @@ if (serve) {
               win32: ["cmd", "/c", "start"],
             };
             if (clients.length === 0) {
-              // @ts-expect-error: `process.platform` could have many other values,
-              //like aix, android, haiku, openbsd, freebsd, etc.
-              spawn(op[process.platform][0], [`http://localhost:3000/examples`]);
+              // @ts-expect-error: `process.platform` could have many other
+              // values, like aix, android, haiku, openbsd, freebsd, etc.
+              spawn(op[process.platform][0], [
+                `http://localhost:3000/examples`,
+              ]);
             }
           }, 1000); //open the default browser only if it is not opened yet
         }
@@ -275,7 +274,7 @@ if (serve) {
 }
 
 if (!watch && !serve) {
-  Object.values(buildmap).forEach(build => {
+  Object.values(buildmap).forEach((build) => {
     build.then((context) => {
       context.dispose();
     });
