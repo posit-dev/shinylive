8910d78d271b496cc2beda0946b9c6c36c225e22
George Stagg <george.stagg@posit.co>
2023-07-25 16:05:59 +0100

    Partially handle webR for OutputCell component

--- src/Components/OutputCell.tsx
+++ src/Components/OutputCell.tsx
@@ -1,4 +1,4 @@
-import { PyodideProxyHandle } from "../hooks/usePyodide";
+import { ProxyHandle } from "./App";
 import { ToHtmlResult } from "../pyodide-proxy";
 import "./OutputCell.css";
 import { TerminalMethods } from "./Terminal";
@@ -8,10 +8,10 @@ import * as React from "react";
 // OutputCell component
 // =============================================================================
 export function OutputCell({
-  pyodideProxyHandle,
+  proxyHandle,
   setTerminalMethods,
 }: {
-  pyodideProxyHandle: PyodideProxyHandle;
+  proxyHandle: ProxyHandle;
   setTerminalMethods: React.Dispatch<React.SetStateAction<TerminalMethods>>;
 }) {
   const [content, setContent] = React.useState<ToHtmlResult>({
@@ -21,10 +21,11 @@ export function OutputCell({
 
   React.useEffect(() => {
     const runCodeInTerminal = async (command: string): Promise<void> => {
-      if (!pyodideProxyHandle.ready) return;
+      if (!proxyHandle.ready) return;
+      if (proxyHandle.engine !== "pyodide") return;
 
       try {
-        const result = await pyodideProxyHandle.pyodide.runPyAsync(command, {
+        const result = await proxyHandle.pyodide.runPyAsync(command, {
           returnResult: "to_html",
           printResult: false,
         });
@@ -39,7 +40,28 @@ export function OutputCell({
       ready: true,
       runCodeInTerminal,
     });
-  }, [setTerminalMethods, pyodideProxyHandle]);
+  }, [setTerminalMethods, proxyHandle]);
+
+  React.useEffect(() => {
+    const runCodeInTerminal = async (command: string): Promise<void> => {
+      if (!proxyHandle.ready) return;
+      if (proxyHandle.engine !== "webr") return;
+
+      try {
+        // TODO: Better convert output of runRAsync into HTML format
+        const result = await proxyHandle.webRProxy.runRAsync(command);
+        const output = JSON.stringify(await result.toJs());
+        setContent({ type: "text", value: output });
+      } catch (e) {
+        setContent({ type: "text", value: (e as Error).message });
+      }
+    };
+
+    setTerminalMethods({
+      ready: true,
+      runCodeInTerminal,
+    });
+  }, [setTerminalMethods, proxyHandle]);
 
   return (
     <div className="shinylive-output-cell">
