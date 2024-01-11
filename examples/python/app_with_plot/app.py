705d20934e9e55609e8959a29a27da1c40422157
Gordon Shotwell <gordon.shotwell@posit.co>
2024-01-11 13:48:42 -0400

    Translate app with plot

--- examples/python/app_with_plot/app.py
+++ examples/python/app_with_plot/app.py
@@ -1,26 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from shiny import App, render, ui
+from shiny import render
+from shiny.express import ui, input
 
-app_ui = ui.page_fluid(
-    ui.layout_sidebar(
-        ui.panel_sidebar(
-            ui.input_slider("n", "N", 0, 100, 20),
-        ),
-        ui.panel_main(
-            ui.output_plot("histogram"),
-        ),
-    ),
-)
+with ui.sidebar():
+    ui.input_slider("n", "N", 0, 100, 20)
 
 
-def server(input, output, session):
-    @output
-    @render.plot(alt="A histogram")
-    def histogram():
-        np.random.seed(19680801)
-        x = 100 + 15 * np.random.randn(437)
-        plt.hist(x, input.n(), density=True)
-
-
-app = App(app_ui, server, debug=True)
+@render.plot(alt="A histogram")
+def histogram():
+    np.random.seed(19680801)
+    x = 100 + 15 * np.random.randn(437)
+    plt.hist(x, input.n(), density=True)
