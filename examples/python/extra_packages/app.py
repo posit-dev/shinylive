609ad7bab2e089d140339ec30080f4be7b359344
Gordon Shotwell <gordon.shotwell@posit.co>
2024-01-12 14:00:32 -0400

    Translate extra packages

--- examples/python/extra_packages/app.py
+++ examples/python/extra_packages/app.py
@@ -1,29 +1,22 @@
 import attrs
 import isodate
 import tabulate
-from shiny import App, ui
+from shiny.express import ui
 
-app_ui = ui.page_fluid(
-    ui.markdown(
-        """
-        This application doesn't actually do anything -- it simply demonstrates how to
-        import extra packages from PyPI, by using a `requirements.txt` file.
+"""
+This application doesn't actually do anything -- it simply demonstrates how to
+import extra packages from PyPI, by using a `requirements.txt` file.
 
-        Packages listed in `requirements.txt` will be installed by micropip just before
-        the app is started. This means that each time someone visits the app, the
-        packages will be downloaded and installed into the browser's Pyodide
-        environment.
+Packages listed in `requirements.txt` will be installed by micropip just before
+the app is started. This means that each time someone visits the app, the
+packages will be downloaded and installed into the browser's Pyodide
+environment.
 
-        If you want test whether a package can be installed this way, either edit
-        `requirements.txt` and reload this app, or try running this in the terminal:
+If you want test whether a package can be installed this way, either edit
+`requirements.txt` and reload this app, or try running this in the terminal:
 
-        ```
-        import micropip
-        await micropip.install("mypackage")
-        import mypackage
-        ```
-        """
-    ),
-)
-
-app = App(app_ui, None)
+```
+import micropip
+await micropip.install("mypackage")
+import mypackage
+"""
