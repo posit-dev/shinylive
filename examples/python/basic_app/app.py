1ba1dc909e4e7841e0b1d0d267d93d4a03f956bc
Gordon Shotwell <gordon.shotwell@posit.co>
2024-01-12 18:51:36 -0400

    Remove apps (#96)
    * Reorganize py-shiny page, remove outdated apps

--- examples/python/basic_app/app.py
+++ examples/python/basic_app/app.py
@@ -1,16 +1,10 @@
-from shiny import App, render, ui
-
-app_ui = ui.page_fluid(
-    ui.input_slider("n", "N", 0, 100, 20),
-    ui.output_text_verbatim("txt"),
-)
+from shiny import render
+from shiny.express import ui, input
 
 
-def server(input, output, session):
-    @output
-    @render.text
-    def txt():
-        return f"n*2 is {input.n() * 2}"
+ui.input_slider("n", "N", 0, 100, 20),
 
 
-app = App(app_ui, server)
+@render.text
+def txt():
+    return f"n*2 is {input.n() * 2}"
--- examples/python/read_local_csv_file/app.py
+++ examples/python/read_local_csv_file/app.py
@@ -1,22 +1,13 @@
 from pathlib import Path
 
 import pandas
-from shiny import App, render, ui
+from shiny import render
 
-app_ui = ui.page_fluid(
-    ui.output_table("table"),
-)
-
-
-def server(input, output, session):
-    @output
-    @render.table
-    def table():
-        infile = Path(__file__).parent / "mtcars.csv"
-        df = pandas.read_csv(infile)
-        # Use the DataFrame's to_html() function to convert it to an HTML table, and
-        # then wrap with ui.HTML() so Shiny knows to treat it as raw HTML.
-        return df
+# We need to import something from express to activate express mode
+import shiny.express
 
 
-app = App(app_ui, server)
+@render.table
+def data_frame():
+    infile = Path(__file__).parent / "mtcars.csv"
+    return pandas.read_csv(infile)
