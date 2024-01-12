efe808557b6cf7b36bcf8ed0a5d1b89c979399d6
Gordon Shotwell <gordon.shotwell@posit.co>
2024-01-12 12:21:14 -0400

    Expressify apps 2 (#92)
    Translates the following apps to Express
    
    * Multiple source files
    
    * Read local csv
    
    * File upload
    
    * Add file download
    
    * Update examples/python/read_local_csv_file/app.py
    
    Co-authored-by: Garrick Aden-Buie <garrick@adenbuie.com>

--- examples/python/file_download/app.py
+++ examples/python/file_download/app.py
@@ -5,71 +5,51 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
-from shiny import App, ui
+from shiny.express import ui, input
+from shiny import render
 
+with ui.layout_columns():
+    with ui.card():
+        ui.card_header("Download a pre-existing file, using its existing name on disk.")
 
-# A card component wrapper.
-def ui_card(title, *args):
-    return (
-        ui.div(
-            {"class": "card mb-4"},
-            ui.div(title, class_="card-header"),
-            ui.div({"class": "card-body"}, *args),
-        ),
-    )
+        @render.download(label="Download CSV", filename="mtcars.csv")
+        def download1():
+            # This is the simplest case. The implementation simply returns the path to a
+            # file on disk.
+            path = Path(__file__).parent / "mtcars.csv"
+            return str(path)
 
+    with ui.card():
+        ui.card_header("Download a PNG that is generated dynamically.")
+        ui.input_text("title", "Plot title", "Random scatter plot")
+        ui.input_slider("num_points", "Number of data points", 1, 100, 50)
 
-app_ui = ui.page_fluid(
-    ui_card(
-        "Download a pre-existing file, using its existing name on disk.",
-        ui.download_button("download1", "Download CSV"),
-    ),
-    ui_card(
-        "Download a PNG that is generated dynamically.",
-        ui.input_text("title", "Plot title", "Random scatter plot"),
-        ui.input_slider("num_points", "Number of data points", 1, 100, 50),
-        ui.download_button("download2", "Download PNG"),
-    ),
-    ui_card(
-        "Download a file with name that is generated dynamically.",
-        ui.download_button("download3", "Download CSV"),
-    ),
-)
+        @render.download(label="Download PNG", filename="image.png")
+        def download2():
+            # Another way to implement a file download is by yielding bytes; either all at
+            # once, like in this case, or by yielding multiple times. When using this
+            # approach, you should pass a filename argument to @session.download, which
+            # determines what the browser will name the downloaded file.
+            x = np.random.uniform(size=input.num_points())
+            y = np.random.uniform(size=input.num_points())
+            plt.figure()
+            plt.scatter(x, y)
+            plt.title(input.title())
+            with io.BytesIO() as buf:
+                plt.savefig(buf, format="png")
+                yield buf.getvalue()
 
+    with ui.card():
+        ui.card_header("Download a file with name that is generated dynamically.")
 
-def server(input, output, session):
-    @session.download()
-    def download1():
-        # This is the simplest case. The implementation simply returns the path to a
-        # file on disk.
-        path = Path(__file__).parent / "mtcars.csv"
-        return str(path)
-
-    @session.download(filename="image.png")
-    def download2():
-        # Another way to implement a file download is by yielding bytes; either all at
-        # once, like in this case, or by yielding multiple times. When using this
-        # approach, you should pass a filename argument to @session.download, which
-        # determines what the browser will name the downloaded file.
-        x = np.random.uniform(size=input.num_points())
-        y = np.random.uniform(size=input.num_points())
-        plt.figure()
-        plt.scatter(x, y)
-        plt.title(input.title())
-        with io.BytesIO() as buf:
-            plt.savefig(buf, format="png")
-            yield buf.getvalue()
-
-    @session.download(
-        filename=lambda: f"data-{date.today().isoformat()}-{np.random.randint(100,999)}.csv"
-    )
-    async def download3():
-        # This version uses a function to generate the filename. It also yields data
-        # multiple times.
-        await asyncio.sleep(0.25)
-        yield "one,two,three\n"
-        yield "新,1,2\n"
-        yield "型,4,5\n"
-
-
-app = App(app_ui, server)
+        @render.download(
+            label="Dynamic file name",
+            filename=lambda: f"data-{date.today().isoformat()}-{np.random.randint(100,999)}.csv",
+        )
+        async def download3():
+            # This version uses a function to generate the filename. It also yields data
+            # multiple times.
+            await asyncio.sleep(0.25)
+            yield "one,two,three\n"
+            yield "新,1,2\n"
+            yield "型,4,5\n"
--- examples/python/file_upload/app.py
+++ examples/python/file_upload/app.py
@@ -2,57 +2,13 @@ import mimetypes
 from math import ceil
 from typing import List
 
-from shiny import App, render, ui
-
-app_ui = ui.page_fluid(
-    ui.input_file("file1", "Choose a file to upload:", multiple=True),
-    ui.input_radio_buttons("type", "Type:", ["Binary", "Text"]),
-    ui.output_text_verbatim("file_content"),
-)
+from shiny import render
+from shiny.express import ui, input
 
 
-def server(input, output, session):
-    MAX_SIZE = 50000
-
-    @output
-    @render.text
-    def file_content():
-        file_infos = input.file1()
-        if not file_infos:
-            return
-
-        # file_infos is a list of dicts; each dict represents one file. Example:
-        # [
-        #   {
-        #     'name': 'data.csv',
-        #     'size': 2601,
-        #     'type': 'text/csv',
-        #     'datapath': '/tmp/fileupload-1wnx_7c2/tmpga4x9mps/0.csv'
-        #   }
-        # ]
-        out_str = ""
-        for file_info in file_infos:
-            out_str += (
-                "=" * 47
-                + "\n"
-                + file_info["name"]
-                + "\nMIME type: "
-                + str(mimetypes.guess_type(file_info["name"])[0])
-            )
-            if file_info["size"] > MAX_SIZE:
-                out_str += f"\nTruncating at {MAX_SIZE} bytes."
-
-            out_str += "\n" + "=" * 47 + "\n"
-
-            if input.type() == "Text":
-                with open(file_info["datapath"], "r") as f:
-                    out_str += f.read(MAX_SIZE)
-            else:
-                with open(file_info["datapath"], "rb") as f:
-                    data = f.read(MAX_SIZE)
-                    out_str += format_hexdump(data)
-
-        return out_str
+MAX_SIZE = 50000
+ui.input_file("file1", "Choose a file to upload:", multiple=True),
+ui.input_radio_buttons("type", "Type:", ["Text", "Binary"]),
 
 
 def format_hexdump(data: bytes) -> str:
@@ -73,4 +29,41 @@ def group_into_blocks(x: List[str], blocksize: int):
     ]
 
 
-app = App(app_ui, server)
+@render.text
+def file_content():
+    file_infos = input.file1()
+    if not file_infos:
+        return
+
+    # file_infos is a list of dicts; each dict represents one file. Example:
+    # [
+    #   {
+    #     'name': 'data.csv',
+    #     'size': 2601,
+    #     'type': 'text/csv',
+    #     'datapath': '/tmp/fileupload-1wnx_7c2/tmpga4x9mps/0.csv'
+    #   }
+    # ]
+    out_str = ""
+    for file_info in file_infos:
+        out_str += (
+            "=" * 47
+            + "\n"
+            + file_info["name"]
+            + "\nMIME type: "
+            + str(mimetypes.guess_type(file_info["name"])[0])
+        )
+        if file_info["size"] > MAX_SIZE:
+            out_str += f"\nTruncating at {MAX_SIZE} bytes."
+
+        out_str += "\n" + "=" * 47 + "\n"
+
+        if input.type() == "Text":
+            with open(file_info["datapath"], "r") as f:
+                out_str += f.read(MAX_SIZE)
+        else:
+            with open(file_info["datapath"], "rb") as f:
+                data = f.read(MAX_SIZE)
+                out_str += format_hexdump(data)
+
+    return out_str
