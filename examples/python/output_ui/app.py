ddfef95916a3fd216d87d164312105efeca5225d
Gordon Shotwell <gordon.shotwell@posit.co>
2024-01-22 14:24:58 -0400

    Add @expressify

--- examples/python/output_ui/app.py
+++ examples/python/output_ui/app.py
@@ -1,5 +1,5 @@
 from shiny import render
-from shiny.express import ui, input, output
+from shiny.express import ui, input, output, expressify
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -7,18 +7,25 @@ import matplotlib.pyplot as plt
 ui.input_slider("card_n", "Number of cards", value=3, min=1, max=5)
 
 
+@expressify
+def custom_card(id):
+    id = id + 1
+    with ui.card():
+        f"Card {id}"
+
+        # Specifying the ID like this lets us include a renderer in the iterator
+        # without causing ID conflicts.
+        @output(id=f"hist_{id }")
+        @render.plot(alt="A histogram")
+        def histogram():
+            np.random.seed(19680801)
+            x = 100 + 15 * np.random.randn(437)
+            plt.hist(x, 20, density=True)
+
+
 @render.express
 def cards():
     with ui.layout_columns():
         for i in range(input.card_n()):
             with ui.card():
-                f"Card {i}"
-
-                # Specifying the ID like this lets us include a renderer in the iterator
-                # without causing ID conflicts.
-                @output(id=f"hist_{i}")
-                @render.plot(alt="A histogram")
-                def histogram():
-                    np.random.seed(19680801)
-                    x = 100 + 15 * np.random.randn(437)
-                    plt.hist(x, 20, density=True)
+                custom_card(i)
