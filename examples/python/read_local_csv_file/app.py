2b29e78de99d861c0b5df54f7dca69c56ac809b5
Carson <cpsievert1@gmail.com>
2024-01-29 14:14:50 -0600

    Express version of read csv (with data_frame version as well)

--- examples/python/read_local_csv_file/app.py
+++ examples/python/read_local_csv_file/app.py
@@ -1,22 +1,26 @@
 from pathlib import Path
 
 import pandas
-from shiny import App, render, ui
 
-app_ui = ui.page_fluid(
-    ui.output_table("table"),
-)
+from shiny import reactive
+from shiny.express import render, ui
 
 
-def server(input, output, session):
-    @output
-    @render.table
-    def table():
-        infile = Path(__file__).parent / "mtcars.csv"
-        df = pandas.read_csv(infile)
-        # Use the DataFrame's to_html() function to convert it to an HTML table, and
-        # then wrap with ui.HTML() so Shiny knows to treat it as raw HTML.
-        return df
+@reactive.calc
+def dat():
+    infile = Path(__file__).parent / "mtcars.csv"
+    return pandas.read_csv(infile)
 
 
-app = App(app_ui, server)
+with ui.navset_card_underline():
+
+    with ui.nav_panel("Data frame"):
+        @render.data_frame
+        def frame():
+            # Give dat() to render.DataGrid to customize the grid
+            return dat()
+
+    with ui.nav_panel("Table"):
+        @render.table
+        def table():
+            return dat()
