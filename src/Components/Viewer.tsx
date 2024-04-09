5290904a5a7283e0ef0883bb028782723c9b872b
Winston Chang <winston@posit.co>
2024-04-09 12:18:54 -0500

    Enable dev mode error console for Python
    This enables the dev mode error console for Py Shiny apps,
    except when the mode is viewer-only.

--- src/Components/Viewer.tsx
+++ src/Components/Viewer.tsx
@@ -126,9 +126,11 @@ async function resetRAppFrame(
 export function Viewer({
   proxyHandle,
   setViewerMethods,
+  devMode = false,
 }: {
   proxyHandle: ProxyHandle;
   setViewerMethods: React.Dispatch<React.SetStateAction<ViewerMethods>>;
+  devMode?: boolean;
 }) {
   const viewerFrameRef = React.useRef<HTMLIFrameElement>(null);
   const [appRunningState, setAppRunningState] = React.useState<
@@ -268,6 +270,7 @@ export function Viewer({
         await pyodideproxy.callPyAsync({
           fnName: ["_start_app"],
           args: [appName],
+          kwargs: { dev_mode: devMode },
         });
 
         viewerFrameRef.current.src = appInfo.urlPath;
--- src/hooks/usePyodide.tsx
+++ src/hooks/usePyodide.tsx
@@ -518,7 +518,8 @@ class ShinyExpressAppModule:
         import shiny.express
         self.app = shiny.express.wrap_express_app(app_path)
 
-async def _start_app(app_name, scope = _shiny_app_registry):
+async def _start_app(app_name, scope = _shiny_app_registry, dev_mode = False):
+    import os
     import sys
     import importlib
     import shiny.express
@@ -537,10 +538,17 @@ async def _start_app(app_name, scope = _shiny_app_registry):
     class ModuleApp:
         app = None
 
+    if dev_mode:
+        # Enable shiny dev mode for error console
+        os.environ["SHINY_DEV_MODE"] = "1"
+
     if shiny.express.is_express_app("app.py", app_dir):
         app_obj = ShinyExpressAppModule(Path(app_dir) / "app.py")
     else:
         app_obj = importlib.import_module(f"{app_name}.app")
+
+    if dev_mode:
+        os.environ.pop("SHINY_DEV_MODE")
 
     scope[app_name] = app_obj
 
