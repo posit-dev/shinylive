ad1b50dfc76ad2db01ec8d8199741817e23e0002
Winston Chang <winston@posit.co>
2024-07-16 22:57:47 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,64 +1,83 @@
 {
   description = "Shinylive web assets";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems (system:
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
           f rec {
             pkgs = import nixpkgs { inherit system; };
             inherit system;
-          });
+          }
+        );
 
-    in {
-      packages = forEachSupportedSystem ({ pkgs, system, ... }: {
-        default = pkgs.stdenv.mkDerivation {
-          name = "shinylive";
-          src = ./.;
+    in
+    {
+      packages = forEachSupportedSystem (
+        { pkgs, system, ... }:
+        {
+          default = pkgs.stdenv.mkDerivation {
+            name = "shinylive";
+            src = ./.;
 
-          # TODO:
-          # - cache yarn packages
-          # - cache pyodide tarball
+            # TODO:
+            # - cache yarn packages
+            # - cache pyodide tarball
 
-          nativeBuildInputs = with pkgs; [
-            nodejs_20
+            nativeBuildInputs = with pkgs; [
+              nodejs_20
 
-            python311
-            (with python311Packages; [ pip virtualenv ])
+              python311
+              (with python311Packages; [
+                pip
+                virtualenv
+              ])
 
-            curl
-            cacert
-            git
-          ];
+              curl
+              cacert
+              git
+            ];
 
-          buildPhase = ''
-            export HOME=$PWD
-            make all
-          '';
+            buildPhase = ''
+              export HOME=$PWD
+              make all
+            '';
 
-          installPhase = ''
-            mkdir -p $out
-            cp -r build/* $out
-          '';
-        };
-      });
+            installPhase = ''
+              mkdir -p $out
+              cp -r build/* $out
+            '';
+          };
+        }
+      );
 
-      devShells = forEachSupportedSystem ({ pkgs, system }: {
-        default = pkgs.mkShell {
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShell {
 
-          # Get the nativeBuildInputs from packages.default
-          inputsFrom = [ self.packages.${system}.default ];
+            # Get the nativeBuildInputs from packages.default
+            inputsFrom = [ self.packages.${system}.default ];
 
-          packages = with pkgs;
-            [
+            packages = with pkgs; [
               nodePackages.npm-check-updates
 
             ];
-        };
-      });
+          };
+        }
+      );
     };
 }
