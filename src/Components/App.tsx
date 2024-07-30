41156111241c158ad77b5961473bc165b4967132
Joe Cheng <joe@rstudio.com>
2024-07-30 10:07:45 -0700

    Change name of message type

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -333,7 +333,7 @@ export function App({
   useRunOnceOnMount(() => {
     if (window.parent === window) return;
 
-    window.parent.postMessage({ type: "shinyliveAppReady" }, "*");
+    window.parent.postMessage({ type: "shinyliveReady" }, "*");
   });
 
   const [utilityMethods, setUtilityMethods] = React.useState<UtilityMethods>({
