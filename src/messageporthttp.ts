f57cd7f583078d35c37e624b354ccf211139d99c
George Stagg <george.stagg@posit.co>
2025-08-26 23:03:25 +0100

    Apply credentialless COEP headers to py shinylive

--- src/messageporthttp.ts
+++ src/messageporthttp.ts
@@ -211,7 +211,13 @@ function asgiHeadersToRecord(headers: any): Record<string, string> {
   headers = headers.map(([key, val]: [Uint8Array, Uint8Array]) => {
     return [uint8ArrayToString(key), uint8ArrayToString(val)];
   });
-  return Object.fromEntries(headers);
+  return Object.assign(
+    {
+      "cross-origin-embedder-policy": "credentialless",
+      "cross-origin-resource-policy": "cross-origin",
+    },
+    Object.fromEntries(headers)
+  );
 }
 
 function asgiBodyToArray(body: any): Uint8Array {
